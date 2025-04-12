043adcac0c7a4b8b67913adb2a81d588c9ffe782
carloscmarcelino <carloslima1598@gmail.com>
2025-04-12 15:13:47 -0300

    fix: sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import React from 'react';
 
 import { SidebarContent } from './SidebarContent';
@@ -8,20 +8,7 @@ import { SidebarContent } from './SidebarContent';
 export const Sidebar = () => {
   const session = useSession();
 
-  console.log('session --> ', session);
-
-  if (!session.data?.user?.name)
-    return (
-      <>
-        <button
-          onClick={() => {
-            signOut();
-          }}
-        >
-          sair
-        </button>
-      </>
-    );
+  if (!session.data?.user?.name) return <></>;
 
   return <SidebarContent session={session.data} />;
 };
